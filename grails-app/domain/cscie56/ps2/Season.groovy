044e3a90c8dc52b839bc2473c74955412ed99974
Michael Regert <mjr171@g.harvard.edu>
2017-02-21 17:31:29 -0600

    Season attributes added and tests added to spec file

--- grails-app/domain/cscie56/ps2/Season.groovy
+++ grails-app/domain/cscie56/ps2/Season.groovy
@@ -2,6 +2,17 @@ package cscie56.ps2
 
 class Season {
 
+    String name
+    Date startDate
+    Date endDate
+
+    static hasMany = [games : Game, leagues : League]
+
     static constraints = {
+        name nullable: false, unique: true
+        games minSize: 0, maxSize: 82 // 0 if season hasn't started yet, max 82 for NBA Season only
+        leagues minSize: 1
+        startDate nullable: false
+        endDate nullable: false
     }
 }
--- src/test/groovy/cscie56/ps2/SeasonSpec.groovy
+++ src/test/groovy/cscie56/ps2/SeasonSpec.groovy
@@ -1,5 +1,6 @@
 package cscie56.ps2
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,6 +8,7 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Season)
+@Mock([Game, League])
 class SeasonSpec extends Specification {
 
     def setup() {
@@ -15,8 +17,59 @@ class SeasonSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "Test that a Season created with no attributes is invalid" () {
+        when:
+        Season s1 = new Season()
+        s1.save(flush: true)
+        then:
+        !s1.validate()
+    }
+
+    //-------------------------------------------------------------------
+
+    void "Test that a Season created with no start date is invalid" () {
+        when:
+        Season s1 = new Season(name: '2017', endDate: new Date())
+        s1.save(flush: true)
+        then:
+        !s1.validate()
+    }
+
+    //-------------------------------------------------------------------
+
+    void "Test that a Season created with no end date is invalid" () {
+        when:
+        Season s1 = new Season(name: '2017', startDate: new Date())
+        s1.save(flush: true)
+        then:
+        !s1.validate()
+    }
+
+    //-------------------------------------------------------------------
+
+    void "Test that a Season cannot have more than 82 games" () {
+        when:
+        def games = []
+        for (int i=0; i<90; i++) {
+            games << new Game()
+        }
+        Season s1 = new Season(name: '2017', startDate: new Date(), games: games)
+        s1.save(flush: true)
+        then:
+        !s1.validate()
+    }
+
+    //-------------------------------------------------------------------
+
+    void "Test that name is unique" () {
+        when:
+        Season s1 = new Season(name: '2017', startDate: new Date(), endDate: new Date())
+        s1.save(flush: true)
+        Season s2 = new Season(name: '2017', startDate: new Date(), endDate: new Date())
+        s2.save()
+
+        then:
+        s1.validate()
+        !s2.validate()
     }
 }
