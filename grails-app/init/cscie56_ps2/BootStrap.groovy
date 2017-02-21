5b90d1c91a595dcf40cc645fba50ba5d0e802b95
Michael Regert <mjr171@g.harvard.edu>
2017-02-21 17:35:32 -0600

    Create Season in BootStrap file successfully

--- grails-app/init/cscie56_ps2/BootStrap.groovy
+++ grails-app/init/cscie56_ps2/BootStrap.groovy
@@ -1,8 +1,15 @@
 package cscie56_ps2
 
+import cscie56.ps2.Season
+
 class BootStrap {
 
     def init = { servletContext ->
+        // Crate a new Season
+        def startDate = Date.parse("yyyy-MM-dd", "2017-01-01")
+        def endDate =  Date.parse("yyyy-MM-dd", "2017-06-01")
+        Season s = new Season(name: '2017', startDate: startDate, endDate: endDate)
+        s.save(flush: true)
     }
     def destroy = {
     }
