fd57026107f5d2f0e8d10c20096873457731cb09
Michael Regert <mjr171@g.harvard.edu>
2017-02-21 20:40:11 -0600

    Added scaffold controllers and populated BootStrap

---
+++ grails-app/controllers/cscie56/ps2/ConferenceController.groovy
@@ -0,0 +1,7 @@
+package cscie56.ps2
+
+class ConferenceController {
+
+    static scaffold = Conference
+
+}
--- grails-app/init/cscie56_ps2/BootStrap.groovy
+++ grails-app/init/cscie56_ps2/BootStrap.groovy
@@ -1,5 +1,7 @@
 package cscie56_ps2
 
+import cscie56.ps2.Conference
+import cscie56.ps2.League
 import cscie56.ps2.Season
 
 class BootStrap {
@@ -8,9 +10,20 @@ class BootStrap {
         // Crate a new Season
         def startDate = Date.parse("yyyy-MM-dd", "2017-01-01")
         def endDate =  Date.parse("yyyy-MM-dd", "2017-06-01")
-        def season = new Season(name: '2017', startDate: startDate, endDate: endDate)
-        season.save(flush: true)
+        def s = new Season(name: '2017', startDate: startDate, endDate: endDate)
+        s.save(flush: true)
+
+        // Create two new Conferences
+        Conference c1 = new Conference(name: 'Western Conference')
+        Conference c2 = new Conference(name: 'Eastern Conference')
+        c1.save()
+        c2.save()
+
+        // Create a new League
+        League l = new League(season: s, conferences: [c1, c2])
+        l.save(flush: true)
     }
+
     def destroy = {
     }
 }
